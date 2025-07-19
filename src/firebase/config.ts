b65107de378f6b4169d3250b733c30f1943c28a5
Omer Faruk Polat <omer.polat@commencis.com>
2025-07-19 15:36:34 +0300

    chore: update VCS configuration and remove debug logs from Firebase config

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -12,17 +12,6 @@ const firebaseConfig = {
   measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 
-// Debug: Environment variables'ları kontrol et
-console.log('Environment Variables:', {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID,
-  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
-});
-
 // Firebase'i başlat
 const app = initializeApp(firebaseConfig);
 
@@ -30,10 +19,5 @@ const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
 
-// Debug için Firestore bağlantısını logla
-console.log('Firebase başlatıldı:', {
-  projectId: firebaseConfig.projectId,
-  authDomain: firebaseConfig.authDomain
-});
 
-export default app; 
+export default app;
